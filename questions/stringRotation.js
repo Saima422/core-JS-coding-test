9f5a3cd4fd9e0ac52a95799031678829f2861a5b
saima <sayedssaima04@gmail.com>
2021-08-03 13:06:57 +0530

    Q6. stringRotation => All test cases passed

--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -1,14 +1,35 @@
-const rotateString = (str, n, direction) => {
-  // str = input string
-  // n = number of rotations
-  //
-  // input str = random ,1, right
-  // output mrando
-};
-
-module.exports = rotateString;
-
 // 18) Check input 
 // 19) Checks for invalid direction
 // 20) Left rotation
 // 21) Right rotation
+
+ // str = input string
+// n = number of rotations
+//
+// input str = random ,1, right
+// output mrando
+
+const rotateString = (str, n, direction) => {
+
+  let result;
+
+  if(Number.isInteger(str) || !Number.isInteger(n)){
+    throw new Error("Invalid Input");
+  }
+
+  let validDir = ['left', 'right'];
+
+  if(!validDir.includes(direction)){
+    throw new Error('Invalid Direction');
+  }
+
+  if(direction == 'left'){
+    result = str.substr(n) + str.substr(0, n);
+  }
+  else{
+    result = str.substr(-n) + str.substr(0, str.length-n);
+  }
+  return result;
+};
+
+module.exports = rotateString;
